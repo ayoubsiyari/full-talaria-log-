7f6a8cc04a4028e210372448e073e6431ff1a336
HADES <hades@talaria.app>
2026-03-23 09:53:02 +0100

    settin

---
+++ extract_func2.ps1
@@ -0,0 +1,4 @@
+$lines = [System.IO.File]::ReadAllLines('c:\Users\HADES\OneDrive\Desktop\talaria\chart v 1.4\chart\index.html')
+$line = $lines[54138]
+$line | Out-File 'c:\Users\HADES\OneDrive\Desktop\talaria\tmp_func2.txt' -Encoding UTF8
+Write-Output "Done. Length: $($line.Length)"
