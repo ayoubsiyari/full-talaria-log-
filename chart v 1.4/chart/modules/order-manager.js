05741e856ff3a9e84e5c3f5d88f46ea331bc33c1
HADES <hades@talaria.app>
2026-04-01 11:35:20 +0100

    order levels

--- chart v 1.4/chart/modules/order-manager.js
+++ chart v 1.4/chart/modules/order-manager.js
@@ -16308,12 +16308,13 @@ class OrderManager {
                 const yAxisWidth = 70;
                 
                 // Position close button (rightmost, before Y-axis area)
-                closeBtn.attr('transform', `translate(${chart.w - yAxisWidth - 15}, ${clampedY})`);
+                const labelRightNudge = 35;
+                closeBtn.attr('transform', `translate(${chart.w - yAxisWidth - 15 + labelRightNudge}, ${clampedY})`);
                 
                 // Position label box to the left of close button
                 const labelTextBbox = labelText.node().getBBox();
                 const labelBoxWidth = labelTextBbox.width + 20;
-                const labelBoxX = chart.w - yAxisWidth - 30 - labelBoxWidth;
+                const labelBoxX = chart.w - yAxisWidth - 30 - labelBoxWidth + labelRightNudge;
                 
                 labelBox
                     .attr('x', labelBoxX)
@@ -18413,7 +18414,7 @@ class OrderManager {
                     const boxHeight = 18;
                     const boxY = y - boxHeight / 2;
                     const yAxisWidth = 70;
-                    const labelRightNudge = isPending ? 0 : 18;
+                    const labelRightNudge = isPending ? 35 : 18;
 
                     closeBtn.attr('transform', `translate(${ch.w - yAxisWidth - 15 + labelRightNudge}, ${y})`);
 
