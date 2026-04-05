968b488a328af258f30d6101d7a7bcbe645ff41f
HADES <hades@talaria.app>
2026-04-05 11:16:45 +0100

    orders line sstyle
    Made-by: siyariayoub

--- chart v 1.4/chart/modules/order-manager.js
+++ chart v 1.4/chart/modules/order-manager.js
@@ -25873,7 +25873,7 @@ class OrderManager {
         ch.svg.selectAll('.exec-order-connector').remove();
         const yScale = ch.scales.yScale;
         const yAxisWidth = ch.margin?.r || 70;
-        const connX = ch.w - yAxisWidth + 4;
+        const connX = ch.w - yAxisWidth - 6;
 
         for (const pos of this.openPositions) {
             if (!this._positionTickerMatchesChartSymbol(pos, ch)) continue;
