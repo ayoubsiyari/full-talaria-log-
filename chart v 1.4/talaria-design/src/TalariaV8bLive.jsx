ffc8d664af9c4d5bfa4c5a7bf9dec576102726a4
siyari ayoub <siyariayoub@gmail.com>
2026-05-05 10:17:46 +0200

    sidebar

--- chart v 1.4/talaria-design/src/TalariaV8bLive.jsx
+++ chart v 1.4/talaria-design/src/TalariaV8bLive.jsx
@@ -8462,7 +8462,7 @@ const TalariaV8bLive = () => {
             width: "100%", height: 32, display: "flex", alignItems: "center", justifyContent: "flex-start",
             background: act ? "rgba(74,106,255,0.08)" : h ? c.hv : "transparent",
             border: "none", cursor: "default", color: pressCol,
-            padding: 0, paddingLeft: railAccentPad, paddingRight: t.dd ? 11 : 4,
+            padding: 0, paddingLeft: railAccentPad + 2, paddingRight: t.dd ? 11 : 4,
             boxSizing: "border-box", touchAction: "manipulation",
             transition: "color 0.15s ease, background 0.12s, padding-left 0.12s", position: "relative", fontFamily: F,
           }}>
@@ -16226,7 +16226,7 @@ const TalariaV8bLive = () => {
         ))}
       </div>
       <div style={{ flex: 1, display: "flex", overflow: "hidden", minWidth: 0 }}>
-        <div data-v9-chrome="1" onPointerDown={(e) => e.stopPropagation()} onMouseDown={(e) => e.stopPropagation()} onClick={(e) => e.stopPropagation()} style={{ width: 36, flexShrink: 0, boxSizing: "border-box", background: c.sf, borderRight: `1px solid rgba(140,160,255,0.22)`, display: "flex", flexDirection: "column", alignItems: "stretch", paddingTop: 1, paddingLeft: 3, paddingRight: 2, overflowY: "auto", overflowX: "hidden" }}>
+        <div data-v9-chrome="1" onPointerDown={(e) => e.stopPropagation()} onMouseDown={(e) => e.stopPropagation()} onClick={(e) => e.stopPropagation()} style={{ width: 36, flexShrink: 0, boxSizing: "border-box", background: c.sf, borderRight: `1px solid rgba(140,160,255,0.22)`, display: "flex", flexDirection: "column", alignItems: "stretch", paddingTop: 1, paddingLeft: 0, paddingRight: 2, overflowY: "auto", overflowX: "hidden" }}>
           {toolGroups.map((group, gi) => (
             <div key={gi} style={{ width: "100%" }}>
               {gi === toolGroups.length - 1 && <div style={{ height: 1, margin: "1px 6px", background: "rgba(140,160,255,0.18)" }}/>}
