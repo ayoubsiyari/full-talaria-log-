3eecd1d7feb9794fa53b35937c57ba69375b412c
siyari ayoub <siyariayoub@gmail.com>
2026-05-05 17:44:50 +0200

    creat session

--- chart v 1.4/Design/vite.config.js
+++ chart v 1.4/Design/vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
+  base: '/talaria-v8b-design/',
+  build: {
+    outDir: path.resolve(__dirname, '../../homepage/public/talaria-v8b-design'),
+    emptyOutDir: true,
+  },
 })
